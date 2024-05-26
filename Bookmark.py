ef63035027f7560105c5bf88624814cfb2829929
hyunholee <dionme2622@gmail.com>
2024-05-27 08:12:30 +0900

    Bookmark 생성

---
+++ Bookmark.py
@@ -0,0 +1,16 @@
+from stdafx import *
+
+class BookmarkWindow:
+
+    def __init__(self):
+        pass
+
+
+    def clear_window(self):
+        for widget in self.master.winfo_children():
+            widget.destroy()
+
+    def go_back(self):
+        self.clear_window()
+        previous_scene = self.scene_stack.pop()
+        previous_scene.__init__(self.master, self.scene_stack)
